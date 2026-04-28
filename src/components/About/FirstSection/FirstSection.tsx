76e8f7c5ffce00016abb97df0ed04498ec251995
TuaneRS <tuanersantana@outlook.com>
2026-04-28 19:06:04 -0300

    incluindo Css

--- src/components/About/FirstSection/FirstSection.tsx
+++ src/components/About/FirstSection/FirstSection.tsx
@@ -1,18 +1,30 @@
 import type { FC } from 'react'
+import styles from './FistSection.module.css'
+
+
+const libraryImageSrc = "src/assets/LibFirstSection.png"
+
 
 const FirstSection: FC = () => {
     return (
-        <section>
-            <div>
-                <h3>EST. MMXXIV</h3>
-                <h1>Um repositório para a Mente Instrospectiva.</h1>
-                <p>Acreditamos que a informação não deve ser apenas armazenada,
-                    mas curada. O Archival Haven é um refúgio digital para aqueles que
-                    valorizam o peso do conhecimento.</p>
-            </div>
-            <div>
-                <img src="" alt="" />
-                <img src="" alt="" />
+        <section className={styles.section}>
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <span className={styles.minorTitle}>EST. MMXXIV</span>
+                    <h1 className={styles.majorTitleBlack}>Um repositório para a </h1>
+                    <h1 className={styles.majorTitleBrown}>Mente Instrospectiva.</h1>
+                    <p className={styles.description}>Acreditamos que a informação não deve ser apenas armazenada,
+                        mas curada. O Archival Haven é um refúgio digital para aqueles que
+                        valorizam o peso do conhecimento.</p>
+                </div>
+                <div className={styles.imageWrapper}>
+                    <div className={`${styles.imageFrame} arch-mask`}>
+                        <img src={libraryImageSrc} alt="Biblioteca clássica" className={styles.image} />
+                    </div>
+                    <div className={styles.quoteCard}>
+                        "A biblioteca é uma esfera cujo centro está em toda parte."
+                    </div>
+                </div>
             </div>
         </section>
     )
